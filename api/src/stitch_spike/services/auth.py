97c7996968fd6d9ac78aa1c7f786462d7726e880
Jackson Hoffart <jackson.hoffart@gmail.com>
2025-10-04 17:58:40 +0100

    update web_api_poc -> stitch_spike

--- api/src/stitch_spike/services/auth.py
+++ api/src/stitch_spike/services/auth.py
@@ -6,13 +6,13 @@ from dotenv import load_dotenv
 # Load environment variables from a .env file
 load_dotenv()
 
-POC_API_KEY = os.getenv("POC_API_KEY")
-POC_API_KEY_NAME = "X-API-Key"
+STITCH_API_KEY = os.getenv("STITCH_API_KEY")
+STITCH_API_KEY_NAME = "X-API-Key"
 
-api_key_header = APIKeyHeader(name=POC_API_KEY_NAME, auto_error=True)
+api_key_header = APIKeyHeader(name=STITCH_API_KEY_NAME, auto_error=True)
 
 
-def get_api_key(POC_API_KEY: str = Security(api_key_header)):
-    if POC_API_KEY == POC_API_KEY:
-        return POC_API_KEY
+def get_api_key(STITCH_API_KEY: str = Security(api_key_header)):
+    if STITCH_API_KEY == STITCH_API_KEY:
+        return STITCH_API_KEY
     raise HTTPException(status_code=403, detail="Invalid API Key")
